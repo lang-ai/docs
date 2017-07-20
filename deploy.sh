9e21decef59957693dc6fd1dea25bf67b27cab0a
efueyo <efueyo@sentisis.com>
2017-07-20 16:09:48 +0200

    docs bucket in Ireland

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
 bundle exec middleman build --clean
-aws s3 sync --delete --region=eu-central-1 build s3://docs.lang.ai
+aws s3 sync --delete build s3://docs.lang.ai
